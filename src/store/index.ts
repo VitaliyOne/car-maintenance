a280eb6a049f2da967d608fdaa03c945051dd0eb
VitaliyOne <vitaliy.one1@yandex.ru>
2024-05-10 21:30:09 +0300

    Add hooks AppDispatch and AppSelector

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,3 +8,6 @@ const rootReducer = combineReducers({
 export const store = configureStore({
   reducer: rootReducer
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

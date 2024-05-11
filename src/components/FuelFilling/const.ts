5b5a26516ce0629f8d5011121c384d0ea2ec58ee
VitaliyOne <vitaliy.one1@yandex.ru>
2024-05-12 00:04:30 +0300

    Bug fix

--- src/components/FuelFilling/const.ts
+++ src/components/FuelFilling/const.ts
@@ -2,8 +2,8 @@ import { IFuelForm } from "./types";
 
 export const DEFAULT_FUEL_FORM_DATA: IFuelForm = {
   id: new Date().getTime(),
-  amount: '',
-  price: '',
+  amount: 0,
+  price: 0,
   mileage: '',
   comment: ''
 }
